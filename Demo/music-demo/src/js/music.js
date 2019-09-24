8f5807965e9d9602baa441af414b41ae06134fb3
T <xueyuhu5094@163.com>
2019-09-24 12:32:58 +0800

    基本功能实现，还有部分细节功能待补充

--- Demo/music-demo/src/js/music.js
+++ Demo/music-demo/src/js/music.js
@@ -1,26 +1,19 @@
-/*function $(selector) {
-    return document.querySelector(selector)
-}
-
-function $$(selector) {
-    return document.querySelectorAll(selector)
-}*/
-
 class player {
     constructor(node) {
         this.root = typeof node === 'string' ? document.querySelector(node) : node;
+        //是字符串则获取DOM元素，是对象的话则直接当作对象对待
         this.$ = selector => this.root.querySelector(selector);
         this.$$ = selector => this.root.querySelectorAll(selector);
         this.music_list = [];
-        this.index = 0;
+        this.music_index = 0;
+        this.page_index = 0;
         this.start();
-        this.audio = new Audio();
+        this.audio = new Audio();//可理解为DOM内的<audio id="audio"></audio>，这样写省去DOM内加东西,因浏览器会禁用自动播放，所以需要给播放按钮加事件
         this.bind();
-        /*
-        * 可理解为，DOM内的<audio id="audio"></audio>，这样写省去DOM内加东西
-        * 为什么要写？浏览器会禁用自动播放，所以需要给播放按钮加事件
-        * */
-
+        /**/
+        this.lyricsArr = [];
+        this.lyricIndex = -1;//存歌词当前到第几行
+        /**/
     }
 
     start() {
@@ -33,17 +26,17 @@ class player {
             )
             .then(
                 (data) => {
-                    console.log(222222);
-                    console.log(data);
-                    this.music_list = data;
-                    this.audio.src = this.music_list[this.index].url;
+                    //console.log(data);
+                    this.music_list = data;//获取音乐，将音乐放到music_list
+                    //this.audio.src = this.music_list[this.music_index].url;
+                    this.rendering_music();
                 }
             )
     }
 
     bind() {
         let that = this;
-        this.root.querySelector('.icon-play').onclick = function () {
+        this.$('.icon-play').onclick = function () {
             //console.log(that.audio);
             if (this.classList.contains('playing')) {
                 that.audio.pause();
@@ -51,7 +44,8 @@ class player {
                 this.classList.add('pause');
                 this.querySelector('use').setAttribute('xlink:href', '#icon-play')
             } else {
-                that.start_play();
+                that.audio.play();
+                //that.start_play();
                 this.classList.remove('pause');
                 this.classList.add('playing');
                 this.querySelector('use').setAttribute('xlink:href', '#icon-pause')
@@ -62,70 +56,214 @@ class player {
         * let that = this;需要绑定到当前点击的播放按钮
         * 下来根据播放按钮状态展示暂停还是播放中（DOM操作，修改SVG的类名）
         * */
-
-        this.root.querySelector('.icon-pre').onclick = function () {//上一曲
-            that.pre_song()
+        this.$('.icon-pre').onclick = function () {//上一曲
+            that.pre_song();
+            that.rendering_music();
         };
-        this.root.querySelector('.icon-next').onclick = function () {//下一曲
-            that.next_song()
+        this.$('.icon-next').onclick = function () {//下一曲
+            that.next_song();
+            that.rendering_music();
         };
-        ////
+        /**/
+        this.audio.ontimeupdate = function () {
+            that.locateLyric();
+            that.setProgerssBar();
+        };
+        /**/
+        //
         let cd = false;
         let intX;
         let newX;
-        this.root.querySelector('.main').ontouchstart = function (e) {
+        this.$('.main').ontouchstart = function (e) {
             intX = e.changedTouches[0].pageX;
-            //console.log(e);
         };
-        this.root.querySelector('.main').ontouchmove = function (e) {
+        this.$('.main').ontouchmove = function (e) {//左右滑动
             newX = e.changedTouches[0].pageX;
             if (!cd) {//加了节流
                 if (newX - intX > 0) {
                     this.classList.remove('main_2');
                     this.classList.add('main_1');
-                    console.log('left')
+                    that.set_page_index();
+                    that.set_spots('spots_selection');
 
                 } else {
                     this.classList.remove('main_1');
                     this.classList.add('main_2');
-                    console.log('right')
+                    that.set_page_index();
+                    that.set_spots('spots_selection');
                 }
                 cd = true;
                 setTimeout(() => {
                     cd = false;
                 }, 500)
             }
-        }
+        };
+        //
     }
 
-    //
-    start_play() {
-        this.audio.oncanplaythrough = () => this.audio.play()
-    }
-
-    /*
-    * 直接使用that.audio.play()会报错，具体原因待详细了解
-    * */
-
-    //
     pre_song() {
-        this.index = (this.music_list.length + this.index - 1) % this.music_list.length;
-        this.audio.src = this.music_list[this.index].url;
+        this.music_index = (this.music_list.length + this.music_index - 1) % this.music_list.length;
+        this.audio.src = this.music_list[this.music_index].url;
         this.audio.oncanplaythrough = () => {
-            this.start_play();
+            //this.start_play();
+            this.audio.play();
         }
     }
 
     next_song() {
-        this.index = (this.music_list.length + this.index + 1) % this.music_list.length;
-        this.audio.src = this.music_list[this.index].url;
+        this.music_index = (this.music_list.length + this.music_index + 1) % this.music_list.length;
+        this.audio.src = this.music_list[this.music_index].url;
         this.audio.oncanplaythrough = () => {
-            this.start_play();
+            //this.start_play();
+            this.audio.play();
         }
     }
 
+    rendering_music() {//获取音乐对象（当前音乐的信息）
+        let music_obj = this.music_list[this.music_index];
+        this.$('.header h1').innerText = music_obj.title;
+        this.$('.header p').innerText = music_obj.author + '_' + music_obj.albumn;
+        this.audio.src = music_obj.url;
+        /**/
+        this.audio.onloadedmetadata = () => {
+            //加载时就获取音乐的总时长
+            this.$('.end_time').innerText = this.formateTime(this.audio.duration);
+        };
+        /**/
+        this.loading_lyric();
 
+    }
+
+    set_spots(class_name) {//当前页面选中原点的状态
+        let len = document.querySelectorAll('.spots>span');
+        for (let i = 0; i < len.length; i++) {
+            len[i].classList.remove(class_name);
+        }
+        len[this.page_index].classList.add(class_name);
+    }
+
+    set_page_index() {//获取当前页面切换在那一个
+        this.$('.main').classList.forEach(
+            (index) => {
+                if (index === 'main_1') {
+                    this.page_index = 0;
+                } else if (index === 'main_2') {
+                    this.page_index = 1;
+                }
+
+            }
+        )
+    }
+
+    loading_lyric() {//加载歌词
+        fetch(this.music_list[this.music_index].lyric)
+            .then(
+                res => res.json()
+            )
+            .then(
+                (data) => {
+                    //console.log(data.lrc.lyric);
+                    /**/
+                    this.setLyrics(data.lrc.lyric);
+                    window.lyrics = data.lrc.lyric;
+                    /**/
+                }
+            )
+    }
+
+
+    /**/
+    setLyrics(lyrics) {//歌词拆分
+        this.lyricIndex = 0;
+        let fragment = document.createDocumentFragment();
+        let lyricsArr = [];
+        this.lyricsArr = lyricsArr;
+        lyrics.split(/\n/)
+            .filter(str => str.match(/\[.+?\]/))
+            .forEach(line => {
+                let str = line.replace(/\[.+?\]/g, '');
+                line.match(/\[.+?\]/g).forEach(t => {
+                    t = t.replace(/[\[\]]/g, '');
+                    let milliseconds = parseInt(t.slice(0, 2)) * 60 * 1000 + parseInt(t.slice(3, 5)) * 1000 + parseInt(t.slice(6));
+                    //milliseconds将歌词是时间转为毫秒
+                    lyricsArr.push([milliseconds, str])
+                })
+            });
+
+        lyricsArr.filter(line => line[1].trim() !== '').sort((v1, v2) => {
+            if (v1[0] > v2[0]) {
+                return 1
+            } else {
+                return -1
+            }
+        }).forEach(line => {
+            let node = document.createElement('p');
+            node.setAttribute('data-time', line[0]);
+            node.innerText = line[1];
+            fragment.appendChild(node)
+        });
+        this.$('.svg-lyrics .svg_lyrics_main').innerHTML = '';
+        this.$('.svg-lyrics .svg_lyrics_main').appendChild(fragment)
+    }
+
+    locateLyric() {//定位播放界面歌词（根据播放时间定位到具体的DOM元素）
+        let currentTime = this.audio.currentTime * 1000;
+        let nextLineTime = this.lyricsArr[this.lyricIndex + 1][0];
+        if (currentTime > nextLineTime && this.lyricIndex < this.lyricsArr.length - 1) {
+            this.lyricIndex++;
+            let node = this.$('[data-time="' + this.lyricsArr[this.lyricIndex][0] + '"]');
+            if (node) this.setLyricToCenter(node);
+            this.$$('.svg-list .lyric p')[0].innerText = this.lyricsArr[this.lyricIndex][1];
+            this.$$('.svg-list .lyric p')[1].innerText = this.lyricsArr[this.lyricIndex + 1] ? this.lyricsArr[this.lyricIndex + 1][1] : '';
+            console.log(this.$$('.svg-list .lyric p')[1].innerText)
+            if(this.$$('.svg-list .lyric p')[1].innerText === ''){
+                console.log('over')
+            }
+
+        }
+    }
+
+    setLyricToCenter(node) {
+        let translateY = node.offsetTop - this.$('.svg-lyrics').offsetHeight / 2;
+        translateY = translateY > 0 ? translateY : 0;
+        this.$('.svg-lyrics .svg_lyrics_main').style.transform = `translateY(-${translateY}px)`;
+        this.$$('.svg-lyrics p').forEach(node => node.classList.remove('lyrics_flg'));
+        node.classList.add('lyrics_flg')
+    }
+
+
+    setProgerssBar() {
+        let percent = (this.audio.currentTime * 100 / this.audio.duration) + '%';
+        this.$('.bar .speed').style.width = percent;
+        this.$('.star_time').innerText = this.formateTime(this.audio.currentTime);
+    }
+
+
+    formateTime(secondsTotal) {
+        let minutes = parseInt(secondsTotal/60);
+        minutes = minutes >= 10 ? '' + minutes : '0' + minutes;
+        let seconds = parseInt(secondsTotal%60);
+        seconds = seconds >= 10 ? '' + seconds : '0' + seconds;
+        return minutes + ':' + seconds
+    }
+
+    /**/
+
+
+    /*
+    *    start_play() {
+    *    this.audio.oncanplaythrough = () => this.audio.play()
+    *    }
+    *   直接使用that.audio.play()会报错，具体原因待详细了解
+    */
 }
 
 
 new player('#player');
+//window.player = new player();
+/*
+* BUG
+* >未点播放按钮，点上一首下一首会自动播放
+* >播放界面的第二行歌词，歌曲播放完毕后没有最后一行了会报错
+* >点击上一首下一首，应先清空之前歌词
+* */
